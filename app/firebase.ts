e075af4ec607c8e976f4634f007e4e9dd32aea82
sany13372 <103900578+sany13372@users.noreply.github.com>
2022-09-12 21:26:23 +0300

    comm

---
+++ app/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app'
+import {
+	getAuth,
+	signOut,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+} from 'firebase/auth'
+import { getFirestore } from '@firebase/firestore'
+
+const firebaseConfig = {}
+
+initializeApp(firebaseConfig)
+
+export const auth = getAuth()
+
+export const register = (email: string, password: string) =>
+	createUserWithEmailAndPassword(auth, email, password)
+
+export const login = (email: string, password: string) =>
+	signInWithEmailAndPassword(auth, email, password)
+
+export const logout = () => signOut(auth)
+
+export const db = getFirestore()
